8fa62ab9278ebfbae1a4c07c41479be2b9085ff5
PapperoniHD <gabrielling@hotmail.se>
2023-09-22 13:55:36 +0200

    Boss first and second phase playable

---
+++ Source/GP4/Enemies/BTTasks/BTTask_StopAnimMontage.cpp
@@ -0,0 +1,32 @@
+﻿// Gabriel Ling Andersson
+
+
+#include "BTTask_StopAnimMontage.h"
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "GP4/Enemies/BaseEnemy.h"
+
+UBTTask_StopAnimMontage::UBTTask_StopAnimMontage(FObjectInitializer const& ObjectInitializer)
+{
+	NodeName = TEXT("Stop Animation Montage");
+
+	// Accept only floats
+	BlackboardKey.AddFloatFilter(this, GET_MEMBER_NAME_CHECKED(UBTTask_StopAnimMontage, BlackboardKey));
+}
+
+EBTNodeResult::Type UBTTask_StopAnimMontage::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	// Get AI Controller & Pawn
+	AAIController* AIController = OwnerComp.GetAIOwner();
+	UBlackboardComponent* BBComponent = AIController->GetBlackboardComponent();
+	ABaseEnemy* AICharacter = Cast<ABaseEnemy>(AIController->GetCharacter());
+
+	
+	AICharacter->StopAnimMontage(AICharacter->GetCurrentMontage());
+
+	
+
+	// Signal the behavior tree component that the task is finished with success
+	FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+	return Super::ExecuteTask(OwnerComp, NodeMemory);
+}
---
+++ Source/GP4/Enemies/BTTasks/BTTask_StopAnimMontage.h
@@ -0,0 +1,22 @@
+﻿// Gabriel Ling Andersson
+
+#pragma once
+
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "BTTask_StopAnimMontage.generated.h"
+
+UCLASS(Blueprintable)
+class UBTTask_StopAnimMontage : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+
+public:
+	UBTTask_StopAnimMontage(FObjectInitializer const& ObjectInitializer);
+
+private:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+	UPROPERTY(BlueprintReadOnly, meta = (AllowPrivateAccess = true))
+	TArray<UAnimMontage*> AnimMontage;
+	
+};
